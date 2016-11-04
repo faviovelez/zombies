87419e72441523e71890b7aabf2d8a09b2963be9
FAVIO VELEZ <favio.velez@hotmail.com>
2016-11-03 21:06:33 -0600

    zombie corregido

--- zombie.rb
+++ zombie.rb
@@ -4,16 +4,18 @@ class Zombie
 	def morder(nombre) 
 		@nombre = nombre
 		puts "Zombie ha mordido a #{@nombre}"
-			def infectar(nombre)
-				puts "Zombie ha convertido a #{@nombre} en zombie"	
-			end
+		infectar(nombre)
+	end
+
+	def infectar(nombre)
+		puts "Zombie ha convertido a #{@nombre} en zombie"	
 	end
 
 	def correr(velocidad)
 		@velocidad = velocidad
 		puts "corro a #{@velocidad} km por hora"
 	end
-	
+
 	def morir
 		puts "he muertooooo"
 	end
